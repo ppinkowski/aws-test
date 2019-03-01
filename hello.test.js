1111d2e96b291f6b0201666f67ba2cd2d9d82b17
Phil <ppinkowski@scottlogic.co.uk>
2019-03-01 15:09:32 +0000

    fix tests

--- hello.test.js
+++ hello.test.js
@@ -2,6 +2,6 @@ const { hello } = require('./hello');
 
 describe("hello", () => {
     it ("works", async () => {
-        expect(await hello({}, {}, ()=> {})).toEqual({statusCode: 200, body: JSON.stringify({hello:"World!"})});
+        expect(await hello({}, {}, ()=> {})).toEqual({statusCode: 200, body: JSON.stringify({yo:"Bitches!"})});
     })
 })
