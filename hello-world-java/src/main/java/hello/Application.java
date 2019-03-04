88dba5a673f5a0303113350c5817af2ef2f089c1
Phil <ppinkowski@scottlogic.co.uk>
2019-03-04 14:21:34 +0000

    Add Java function

---
+++ hello-world-java/src/main/java/hello/Application.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import({HelloHandler.class})
+public class Application extends SpringBootServletInitializer {
+
+    // silence console logging
+    @Value("${logging.level.root:OFF}")
+    String message = "";
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
