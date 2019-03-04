f9a6fc6fadc012807f095d2dd74c66072d651104
Phil <ppinkowski@scottlogic.co.uk>
2019-03-04 16:56:20 +0000

    Try alternate handler

--- hello-world-java/src/main/java/hello/HelloHandler.java
+++ hello-world-java/src/main/java/hello/HelloHandler.java
@@ -1,32 +1,14 @@
 package hello;
 
-import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
-import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public class HelloHandler implements RequestStreamHandler {
-
-    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-    static {
-        try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
-        } catch (ContainerInitializationException e) {
-            // if we fail here. We re-throw the exception to force another cold start
-            e.printStackTrace();
-            throw new RuntimeException("Could not initialize Spring Boot application", e);
-        }
-    }
+public class HelloHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
 
     @Override
-    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-        System.out.println("Received Request!!");
-        handler.proxyStream(input, output, context);
+    public AwsProxyResponse handleRequest(AwsProxyRequest input, Context context) {
+        return new AwsProxyResponse(200, null, "{\"message\": \"Hello from Java!\"}");
     }
 }
