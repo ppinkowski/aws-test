e482f6daedccf6d9eb48373a7c3bad4d0464641e
Phil <ppinkowski@scottlogic.co.uk>
2019-03-05 11:20:20 +0000

    fix call?

--- hello-world-js/app.js
+++ hello-world-js/app.js
@@ -4,11 +4,12 @@ const lambdaClient = new Lambda({ region: 'eu-west-2' });
 
 const invokeLambda = params =>
   new Promise((res, rej) =>
-    lambdaClient.invoke(params, (err, data) => (err ? rej(err) : res(data)))
+    lambdaClient.invoke(params, (err, data) =>
+      err && !data ? rej(err) : res(data)
+    )
   );
 
 exports.handler = async (event, context, callback) => {
-  console.log(process.env);
   const result = await invokeLambda({
     ClientContext: 'JSLambda',
     FunctionName: process.env.JavaFunction,
