42b8a594270ecf3222061df804518f4544ac7011
Phil <ppinkowski@scottlogic.co.uk>
2019-03-05 10:18:58 +0000

    Update lambda to call other lambda

--- hello-world-js/app.js
+++ hello-world-js/app.js
@@ -1,8 +1,22 @@
-exports.handler = async (event, context) => {
-    return {
-        'statusCode': 200,
-        'body': JSON.stringify({
-            message: 'Hello, World!'
-        })
-    };
+const Lambda = require('aws-sdk/clients/lambda');
+
+const lambdaClient = new Lambda({ region: 'eu-west-2' });
+
+const invokeLambda = params =>
+  new Promise(res, rej =>
+    lambdaClient.invoke(params, (err, data) => (err ? rej(err) : res(data)))
+  );
+
+exports.handler = async (event, context, callback) => {
+  const result = await invokeLambda({
+    ClientContext: 'JSLambda',
+    FunctionName: process.env.JavaFunction,
+    InvocationType: 'RequestResponse'
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      callJavaResult: result
+    })
+  };
 };
