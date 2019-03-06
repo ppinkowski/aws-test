a7affdc4360d20f6cd08e2ce30bf1ac838a7d168
Phil <ppinkowski@scottlogic.co.uk>
2019-03-06 16:09:16 +0000

    add second java lambda function

---
+++ lambda-2/src/main/java/hello/LambdaHandler.java
@@ -0,0 +1,14 @@
+package hello;
+
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+
+    @Override
+    public AwsProxyResponse handleRequest(AwsProxyRequest input, Context context) {
+        return new AwsProxyResponse(200, null, "{\"message\": \"Hello from Java handler 2!\"}");
+    }
+}
