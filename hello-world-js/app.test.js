d2d63b8429670ee07c4a0f97ec3bf1b7453cc09b
Phil <ppinkowski@scottlogic.co.uk>
2019-03-06 13:31:37 +0000

    remove java lambda

--- hello-world-js/app.test.js
+++
@@ -1,7 +0,0 @@
-const { handler } = require('./app');
-
-describe('app', () => {
-  it('works', async () => {
-    // expect(await handler({}, {}, ()=> {})).toEqual({statusCode: 200, body: JSON.stringify({message: 'Hello, World!'})});
-  });
-});
